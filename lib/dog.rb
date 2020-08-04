6f104aeeedf0d6c0a26f65e9243492d8765c3669
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-04 16:29:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog 
-  def name 
+  def name=(doge_name)
+    @this_doge = doge_name
+  end
+  def name
+    @this_doge
+  end
+  def breed=(doge_breed)
+    @this_breed = doge_breed
+  end
+  def breed
+    @this_breed
+  end
 end
+
+frodo = Dog.new
+frodo.name = "Frodie"
+frodo.breed = "American Eskimo"
+
+puts frodo.name
+puts frodo.breed
+
