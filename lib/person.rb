823ba71725f633bbf5a69ada3cab1576d7c50278
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-04 16:39:47 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(hooman_name) 
+   @name = hooman_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(hooman_job)
+    @job = hooman_job
+  end
+  
+  def job
+    @job
+  end
+end
