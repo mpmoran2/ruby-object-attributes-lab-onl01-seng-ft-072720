78a0951aec56d6bf5604559bb0f04c074fcbff95
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-04 16:40:01 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  def name=(doge_name) 
+   @name = doge_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(doge_breed)
+    @breed = doge_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
+
+
